a30c05b18e64d2d214b0c9d581f2dd789074cc9d
Eskimo <zhangfangyuan@myhexin.com>
2020-08-10 14:45:12 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>你好</div>
+  <Eskimo/>
+</template>
+
+<script>
+import Eskimo from './components/Eskimo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Eskimo
+  }
+}
+</script>
