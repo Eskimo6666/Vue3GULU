5e42e1abe7c0ffd2d241705e48a7374aa4dedd2e
Eskimo <zhangfangyuan@myhexin.com>
2020-09-01 15:27:16 +0800

    接收value和input事件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>
-        Switch 文档
-    </div>
+<div>
+    <Switch :value="y" @input='y = $event'/>
+</div>
 </template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+import {ref} from 'vue'
+export default {
+    components:{Switch},
+    setup(){
+        const y = ref(false)
+        return {y}
+    }
+}
+</script>
