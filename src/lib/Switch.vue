97c68f36fba542d3f1e5584d2a2e78a988b2dcfd
Eskimo <zhangfangyuan@myhexin.com>
2020-09-02 17:26:31 +0800

    Switch complete

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -5,20 +5,18 @@
 </template>
 
 <script>
-import {ref} from 'vue'
+import { ref } from "vue";
 export default {
-    props:{
-        value:Boolean
-    },
-    setup(props, context){
-        const toggle = () => {
-            // checked.value = !checked.value
-            context.emit('update:value', !props.value)
-        }
-        return {toggle}
-    },
-    
-}
+  props: {
+    value: Boolean
+  },
+  setup(props, context) {
+    const toggle = () => {
+      context.emit("update:value", !props.value);
+    };
+    return { toggle };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -28,28 +26,39 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: grey;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
-}
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
-  transition: left 250ms;
-}
-button.checked{
-    background: blue;
-}
-button.checked > span{
-    left: calc(100% - #{$h2} - 2px)
-}
-button:focus{
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+  }
+  &.checked {
+    background: #1890ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus {
     outline: none;
+  }
+  &:active {
+    > span {
+      width: $h2 + 8px;
+    }
+  }
+  &.checked:active {
+    > span {
+      width: $h2 + 8px;
+      margin-left: -8px;
+    }
+  }
 }
 </style>
 <script>
